10d4c3a778e57b59f841cdb5e8d1d9f61a34a27b
ShawnPatrick-Barhorst <shawnpatrick.barhorst@gmail.com>
2024-08-25 00:46:31 -0400

    first_post

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import pandas as pd
 
 app = Flask(__name__)
 
@@ -8,6 +9,7 @@ posts = [
         'title': 'StreetFighterII - Reinforcement Learning',
         'thumbnail': 'images/thumbnails/streetfighterII.gif',
         'description': 'Training a reinforcement learning agent to play StreetFighterII',
+        'data': '04-6-2024',
         'link': '/post/streetfighterII-RL.html'
     },
 
@@ -15,6 +17,7 @@ posts = [
         'title': 'Old School Runescape Grand Exchange - Time Series Prediction',
         'thumbnail': 'images/thumbnails/OSRS_GE.gif',
         'description': 'Training an LSTM to predict Grand Exchange prices.',
+        'data': '04-6-2024',
         'link': '/post/osrs_ge-lstm.html'
     }
 ]
@@ -35,3 +38,6 @@ def post(link):
 
 
 
+
+if __name__ == '__main__':
+    app.run(debug=True)
